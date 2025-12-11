96fd4572ddd00953b6be19f16f379f19f9a914bf
Mefjut <s204108@student.pg.edu.pl>
2025-12-11 21:21:44 +0100

    Create Glaz.h

---
+++ Glaz.h
@@ -0,0 +1,23 @@
+﻿#pragma once
+
+#include <iostream>
+#include <SFML/Graphics.hpp>
+#include <array>
+
+class Glaz : public sf::RectangleShape
+{
+public:
+    Glaz(sf::Vector2f startPos, sf::Vector2f rozmiar, int wytrzymalosc);
+
+    void otrzymajUderzenie();
+    void odswiezKolor();
+    void rysuj(sf::RenderTarget& target) const;
+
+    bool czyZniszczony() const { return m_czyZniszczony; }
+    int wezWytrzymalosc() const { return m_zycie; }
+
+private:
+    int m_zycie;
+    bool m_czyZniszczony;
+    static const std::array<sf::Color, 4> m_koloryLUT;
+};
